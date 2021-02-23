578edd131a6201693cd83afa40e3bc8ac51e5fad
母佰聪 <m18338933510@163.com>
2021-02-23 18:43:36 +0800

    完成初始化樣式

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,42 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="App">
+    <h1>adsdasdas</h1>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  // 组件名称
+  name:"App",
+  // 局部注册的组件
+  components: {},
+  // 组件参数 接收来自父组件的数据
+  props: {},
+  // 组件状态值
+  data() {
+    return {
 
-#nav {
-  padding: 30px;
+    };
+  },
+  // 计算属性
+  computed: {},
+  // 侦听器
+  watch: {},
+  // 生命周期钩子   注：没用到的钩子请自行删除
+  /**
+  * 组件实例创建完成，属性已绑定，但DOM还未生成，$ el属性还不存在
+  */
+  created() {},
+  /**
+  * el 被新创建的 vm.el 替换，并挂载到实例上去之后调用该钩子。
+  * 如果 root 实例挂载了一个文档内元素，当 mounted 被调用时 vm.el 也在文档内。
+  */
+  mounted() {},
+  // 组件方法
+  methods: {}
+};
+</script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+<style scoped lang="less">
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
